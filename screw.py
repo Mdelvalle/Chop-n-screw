9e2d6164c2654e82d2751450a5628acefad28941
Mdelvalle <mdelvalle@live.com>
2015-08-16 04:23:14 -0500

    Add replay of a section, n times

--- screw.py
+++ screw.py
@@ -17,28 +17,59 @@ def screw(input_filename, output_filename):
     tempo = -20
     pitch = -6
 
+    chance_of_small_chop = 0.05
+    chance_of_large_chop = 0.10
+
+    last_played_index = 0
+
     print 'Slow tempo...'
 
     beats2 = song.analysis.beats[1:]
     old_data = None
 
+    # hold all beats of the newly computed song for easy access later
+    played_song = {}
     # Lower the pitch and the tempo.
     # Have the same beat twice; one of them is one beat ahead of the other.
     # There's a 10% chance of switching between them, which gives
     # the song that 'chopping' feel.
-    for beat, beat2 in zip(beats, beats2):
+    for idx, (beat, beat2) in enumerate(zip(beats, beats2)):
         if old_data is not None:
-            if random.random() < 0.1:
+            if random.random() <= chance_of_small_chop:
                 new_beat = soundtouch.shiftTempoChange(song[beat], tempo)
             else:
                 new_beat = soundtouch.shiftTempoChange(song[beat2], tempo)
         else:
-            if random.random() < 0.1:
+            if random.random() <= chance_of_small_chop:
                 new_beat = soundtouch.shiftPitchSemiTones(song[beat], pitch)
             else:
                 new_beat = soundtouch.shiftPitchSemiTones(song[beat2], pitch)
 
-        out_data.append(new_beat)
+        played_song[idx] = new_beat
+
+        if random.random() < chance_of_large_chop:
+            # Make it go back either 4 or 8 beats.
+            go_back_n_beats = random.randint(1, 2) * 4
+
+            # How many times you're allowed to play this segment.
+            n_time_plays = random.randint(1, 2)
+            
+            # Make sure you aren't going back to any beats that were just played
+            # unless you go to the beginning.
+            if go_back_n_beats <= idx and idx - last_played_index >= 32:
+                temp_beats, temp_plays = go_back_n_beats, n_time_plays
+                while n_time_plays > 0:
+                    while go_back_n_beats > 0:
+                        out_data.append(played_song[idx - go_back_n_beats])
+                        go_back_n_beats -= 1
+                    go_back_n_beats = temp_beats
+                    n_time_plays -= 1
+                n_time_plays = temp_plays
+
+                last_played_index = idx
+        else:
+            out_data.append(new_beat)
+
         old_data = song[beat]
 
     print 'Encoding...'
