a4e29577f41dd85e252052c47066443ad8b1a52d
Mdelvalle <mdelvalle@live.com>
2015-08-18 22:57:17 -0500

    Refactoring

--- screw.py
+++ screw.py
@@ -2,78 +2,88 @@ from echonest.remix import audio, modify
 import random
 
 
-def screw(input_filename, output_filename):
-    """
-    Slow down tempo and lower the pitch
-    """
+class ChopnScrew:
+    def __init__(self, input_filename, output_filename):
+        self.soundtouch = modify.Modify()
+        self.song = audio.LocalAudioFile(input_filename)
+        self.beats = self.song.analysis.beats
+        self.out_shape = len(self.song.data)
+        self.out_data = audio.AudioData(shape=self.out_shape, numChannels=1, sampleRate=44100)
 
-    soundtouch = modify.Modify()
-    song = audio.LocalAudioFile(input_filename)
-    beats = song.analysis.beats
-    out_shape = len(song.data)
-    out_data = audio.AudioData(shape=out_shape, numChannels=1, sampleRate=44100)
+        # Amount to change
+        self.tempo = -20
+        self.pitch = -6
 
-    # Amount to change
-    tempo = -20
-    pitch = -6
+        self.chance_of_small_chop = 0.05
+        self.chance_of_large_chop = 0.10
 
-    chance_of_small_chop = 0.05
-    chance_of_large_chop = 0.10
+        self.last_played_index = 0
 
-    last_played_index = 0
+        self.beats2 = song.analysis.beats[1:]
 
-    print 'Slow tempo...'
+        # hold all beats of the newly computed song for easy access later
+        self.played_song = {}
 
-    beats2 = song.analysis.beats[1:]
-    old_data = None
+        self.screw()
 
-    # hold all beats of the newly computed song for easy access later
-    played_song = {}
-    # Lower the pitch and the tempo.
-    # Have the same beat twice; one of them is one beat ahead of the other.
-    # There's a 10% chance of switching between them, which gives
-    # the song that 'chopping' feel.
-    for idx, (beat, beat2) in enumerate(zip(beats, beats2)):
-        if old_data is not None:
-            if random.random() <= chance_of_small_chop:
-                new_beat = soundtouch.shiftTempoChange(song[beat], tempo)
+    def screw():
+        """
+        Slow down tempo and lower the pitch.
+        """
+        print 'Slow tempo...'
+
+        old_data = None
+
+        # Lower the pitch and the tempo.
+        # Have the same beat twice; one of them is one beat ahead of the other.
+        # There's a 10% chance of switching between them, which gives
+        # the song that 'chopping' feel.
+        for idx, (beat, beat2) in enumerate(zip(self.beats, self.beats2)):
+            if old_data is not None:
+                if random.random() <= self.chance_of_small_chop:
+                    new_beat = self.soundtouch.shiftTempoChange(self.song[beat], self.tempo)
+                else:
+                    new_beat = self.soundtouch.shiftTempoChange(self.song[beat2], self.tempo)
             else:
-                new_beat = soundtouch.shiftTempoChange(song[beat2], tempo)
-        else:
-            if random.random() <= chance_of_small_chop:
-                new_beat = soundtouch.shiftPitchSemiTones(song[beat], pitch)
+                if random.random() <= self.chance_of_small_chop:
+                    new_beat = self.soundtouch.shiftPitchSemiTones(self.song[beat], self.pitch)
+                else:
+                    new_beat = self.soundtouch.shiftPitchSemiTones(self.song[beat2], self.pitch)
+
+            self.played_song[idx] = new_beat
+
+            if random.random() < self.chance_of_large_chop:
+                self.chop(idx)
             else:
-                new_beat = soundtouch.shiftPitchSemiTones(song[beat2], pitch)
+                self.out_data.append(new_beat)
 
-        played_song[idx] = new_beat
+            self.old_data = self.song[beat]
 
-        if random.random() < chance_of_large_chop:
-            # Make it go back either 4 or 8 beats.
-            go_back_n_beats = random.randint(1, 2) * 4
+        print 'Encoding...'
+        out_data.encode(output_filename)
 
-            # How many times you're allowed to play this segment.
-            n_time_plays = random.randint(1, 2)
-            
-            # Make sure you aren't going back to any beats that were just played
-            # unless you go to the beginning.
-            if go_back_n_beats <= idx and idx - last_played_index >= 32:
-                temp_beats, temp_plays = go_back_n_beats, n_time_plays
-                while n_time_plays > 0:
-                    while go_back_n_beats > 0:
-                        out_data.append(played_song[idx - go_back_n_beats])
-                        go_back_n_beats -= 1
-                    go_back_n_beats = temp_beats
-                    n_time_plays -= 1
-                n_time_plays = temp_plays
+    def chop(idx):
+        """
+        Either repeat a beat or repeat a long segment.
+        """
+        # Make it go back either 4 or 8 beats.
+        go_back_n_beats = random.randint(1, 2) * 8
 
-                last_played_index = idx
-        else:
-            out_data.append(new_beat)
+        # How many times you're allowed to play this segment.
+        n_time_plays = random.randint(1, 2)
+        
+        # Make sure you aren't going back to any beats that were just played
+        # unless you go to the beginning.
+        if go_back_n_beats <= idx and idx - self.last_played_index >= 40:
+            temp_beats, temp_plays = go_back_n_beats, n_time_plays
+            while n_time_plays > 0:
+                while go_back_n_beats > 0:
+                    self.out_data.append(self.played_song[idx - go_back_n_beats])
+                    go_back_n_beats -= 1
+                go_back_n_beats = temp_beats
+                n_time_plays -= 1
 
-        old_data = song[beat]
-
-    print 'Encoding...'
-    out_data.encode(output_filename)
+            self.last_played_index = idx
 
 
 def main():
@@ -96,7 +106,7 @@ def main():
         print usage
         sys.exit(-1)
 
-    screw(input_filename, output_filename)
+    chopped_n_screwed = ChopnScrew(input_filename, output_filename)
     
 
 if __name__ == '__main__':
