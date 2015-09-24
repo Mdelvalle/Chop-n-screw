acd34b8fd458bd89c46b5ec6384c5d60334b93c2
Mdelvalle <mdelvalle@utexas.edu>
2015-09-23 23:20:13 -0500

    Black bg, purple letters and button

--- index.js
+++ index.js
@@ -13,71 +13,81 @@ var app = express();
  *  Targt dir is './uploads' and file limit size 10MB.
  */
 var upload = multer({
-    dest: './uploads/',
-    limits: {
-        fieldSize: '15MB'
-    }
+	dest: './uploads/',
+	limits: {
+		fieldSize: '15MB'
+	}
 }).single('file');
 
 function puts(err, stdout, stderr) {
-    if (err) {
-        console.log(stderr);
-        console.log(stdout);
-        console.log(err);
+	if (err) {
+		console.log(stderr);
+		console.log(stdout);
+		console.log(err);
 
-        throw err;
-    }
+		throw err;
+	}
 
-    console.log(stdout);
+	console.log(stdout);
 }
 
-app.set('port', (process.env.PORT || 5005));
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname, '/css'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/upload.html');
 });
 
 app.post('/upload', upload, function(req, res, next) {
-    var username = req.body.username;
-    var tmpPath = req.file.path;
-    var targetPath = './uploads/' + req.file.originalname;
-    var convertedPath = './uploads/' + 'CnS-' + req.file.originalname;
-    mkdirp('./uploads/', function(err, result) {
-        if (err) {
-            throw err;
-        }
+	var username = req.body.username;
+	var tmpPath = req.file.path;
+	var targetPath = './uploads/' + req.file.originalname;
+	var convertedPath = './uploads/' + 'CnS-' + req.file.originalname;
+	mkdirp('./uploads/', function(err, result) {
+		if (err) {
+			throw err;
+		}
 
-        fs.rename(tmpPath, targetPath, function(err) {
-            if (err) {
-                throw err;
-            }
-            res.send("File uploaded to " + targetPath);
+		fs.rename(tmpPath, targetPath, function(err) {
+			if (err) {
+				throw err;
+			}
+			res.send("File uploaded to " + targetPath);
 
-            child_process.execFile('python',
-		   ['screw.py', targetPath, convertedPath],
-		   function(err, stdout, stderr) {
-		       if (err) {
-			   console.log(stderr);
-			   console.log(stdout);
-			   console.log(err);
+			child_process.execFile('python',
+				['screw.py', targetPath, convertedPath],
+				function(err, stdout, stderr) {
+					if (err) {
+						console.log(stderr);
+						console.log(stdout);
+						console.log(err);
 
-			   throw err;
-		       }
+						throw err;
+					}
 
-		       console.log(stdout);
+					console.log(stdout);
 
-		       fs.unlink(targetPath, function(err) {
-			   if (err) {
-			       throw err;
-			   }
-		       });
-	    });
-        });
-    });
+					fs.unlink(targetPath, function(err) {
+						if (err) {
+							throw err;
+						}
+					});
+				});
+		});
+	});
+});
+
+app.post('/download', function(req, res, next) {
+	res.download('./uploads/CnS-3000.mp3');
+});
+
+app.get('/analyzing', function(req, res, next) {
+	res.send('analyzing...');
 });
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
